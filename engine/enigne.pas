2f412ddfc5da01dabb65e7d2e566e9400ed7dfaa
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2025-08-09 13:50:10 +0100

    fixing layout

--- engine/enigne.pas
+++ engine/enigne.pas
@@ -8,9 +8,9 @@ unit enigne;
 interface
 
 uses
-  ekron, vid_h, Sound_h, snd_mix, snd_mem, snd_loc, snd_dma, ref, Qmenu, menu, 
-  keys, Console, Client, cl_view, cl_tent, cl_scrn, cl_pred, cl_parse, 
-  cl_newfx, cl_main, cl_inv, cl_input, cl_fx, cl_ents, cl_cin, asm_i386, 
+  vid_h, Sound_h, snd_mix, snd_mem, snd_loc, snd_dma, ref, Qmenu, menu, keys, 
+  Console, Client, cl_view, cl_tent, cl_scrn, cl_pred, cl_parse, cl_newfx, 
+  cl_main, cl_inv, cl_input, cl_fx, cl_ents, cl_cin, asm_i386, 
   LazarusPackageIntf;
 
 implementation
